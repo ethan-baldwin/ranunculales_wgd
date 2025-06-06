bc96721c4b05a90b9d0cec0745c2b905126d63f5
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-06-06 10:54:37 -0400

    add iqtree array script

---
+++ iqtree_array.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=iqtree
+#SBATCH --partition=batch
+#SBATCH --mail-type=END,FAIL
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=4gb
+#SBATCH --time=1:00:00
+#SBATCH --array=1-5 
+#SBATCH --output=/scratch/eab77806/logs/%x_%j.out
+#SBATCH --error=/scratch/eab77806/logs/%x_%j.err
+
+gene=$(awk "NR==${SLURM_ARRAY_TASK_ID}" genelist.txt)
+
+#create output directory
+OUTDIR="/scratch/eab77806/ranunculales/iqtree"
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+cd $OUTDIR
+
+ml IQ-TREE/2.2.2.6-gompi-2022a
+iqtree2 -s ../alignments/$gene.fa -nt AUTO -bb 1000 -m MFP --prefix $gene
