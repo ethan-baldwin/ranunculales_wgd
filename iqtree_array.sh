09a71f8b4bf091ae5684f456d1c36f79748a5287
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-06-09 09:20:05 -0400

    add time to iqtree and redo finished runs

--- iqtree_array.sh
+++ iqtree_array.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=4gb
-#SBATCH --time=5:00:00
+#SBATCH --time=12:00:00
 #SBATCH --array=1-2000 
 #SBATCH --output=/scratch/eab77806/logs/%x_%j.out
 #SBATCH --error=/scratch/eab77806/logs/%x_%j.err
@@ -21,4 +21,4 @@ fi
 cd $OUTDIR
 
 ml IQ-TREE/2.2.2.6-gompi-2022a
-iqtree2 -s ../alignments/$gene.fa -nt AUTO -bb 1000 -m MFP --prefix $gene
+iqtree2 -s ../alignments/$gene.fa -nt AUTO -bb 1000 -m MFP --prefix $gene --redo-tree
