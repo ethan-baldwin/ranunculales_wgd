2c26dccdeabc709352e82e9b9da4b9ba721b7804
Ethan Baldwin <70599620+ethan-baldwin@users.noreply.github.com>
2025-08-08 14:01:42 -0400

    create alignment and iqtree combo script

---
+++ iqtree_array copy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --job-name=mafft
+#SBATCH --partition=batch
+#SBATCH --mail-type=END,FAIL
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=30gb
+#SBATCH --time=4:00:00
+#SBATCH --array=1-20 
+#SBATCH --output=/scratch/eab77806/logs/%x_%j.out
+#SBATCH --error=/scratch/eab77806/logs/%x_%j.err
+
+gene=$(awk "NR==${SLURM_ARRAY_TASK_ID}" genelist.txt)
+
+#create output directory
+OUTDIR="/scratch/eab77806/jim_projects/ranunculales/"
+if [ ! -d $OUTDIR ]
+then
+    mkdir -p $OUTDIR
+fi
+cd $OUTDIR
+
+# run mafft 
+ml MAFFT/7.526-GCC-13.3.0-with-extensions
+mafft --thread $SLURM_CPUS_PER_TASK --auto fastas4jim_SynHOGs/$gene.fa > alignments/$gene.aln
+
+# make iqtree directory and run iqtree
+if [ ! -d iqtree ]
+then
+    mkdir -p iqtree
+fi
+cd iqtree
+
+ml purge
+ml IQ-TREE/2.3.6-gompi-2023a
+iqtree2 -s alignments/$gene.fa -nt AUTO -bb 1000 -m MFP --prefix $gene --redo-tree
